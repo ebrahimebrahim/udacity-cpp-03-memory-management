e3afe9de3551af67b301c15a7a6d4dd4454832d4
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-11-10 00:16:17 -0500

    make chatBot's image handle a unique ptr

--- src/chatbot.h
+++ src/chatbot.h
@@ -3,6 +3,7 @@
 
 #include <wx/bitmap.h>
 #include <string>
+#include <memory>
 
 class GraphNode; // forward declaration
 class ChatLogic; // forward declaration
@@ -11,7 +12,7 @@ class ChatBot
 {
 private:
     // data handles (owned)
-    wxBitmap *_image; // avatar image
+    std::unique_ptr<wxBitmap> _image; // avatar image
 
     // data handles (not owned)
     GraphNode *_currentNode;
@@ -29,6 +30,7 @@ public:
 
     //// STUDENT CODE
     ////
+    // (Task 2)
 
     ////
     //// EOF STUDENT CODE
@@ -37,7 +39,7 @@ public:
     void SetCurrentNode(GraphNode *node);
     void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
     void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
-    wxBitmap *GetImageHandle() { return _image; }
+    wxBitmap *GetImageHandle() { return _image.get(); }
 
     // communication
     void ReceiveMessageFromUser(std::string message);
